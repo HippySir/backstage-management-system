b4d9f04328bd000f775c8e8e15e6494425fb0f78
HippySir <737438933@qq.com>
2019-03-30 16:03:45 +0800

    登陆验证的完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,33 +1,77 @@
 <template>
   <div id="loginfirst">
     <div id="login">
-       
-      <el-form label-position="top" label-width="80px">
-           <h2>用户登陆</h2>
-        <el-form-item label="用户名">
-          <el-input></el-input>
+      <el-form
+        label-position="top"
+        label-width="80px"
+        :model="ruleLogin"
+        :rules="rules"
+        ref="ruleLogin"
+      >
+        <h2>用户登陆</h2>
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="ruleLogin.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
-          <el-input></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleLogin.password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button>重置</el-button>
+          <el-button type="primary" @click="submitlogin('ruleLogin')">登陆</el-button>
+          <el-button @click="resetLogin('ruleLogin')">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      ruleLogin: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 4, max: 16, message: "长度在 4 到 16 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 5, max: 20, message: "长度在 5 到 20 个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitlogin(ruleLogin) {
+      this.$refs[ruleLogin].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          this.$message({
+            message: "验证没有通过！",
+            type: "warning"
+          });
+          return false;
+        }
+      });
+    },
+    // 重置的方法
+     resetLogin(ruleLogin) {
+        this.$refs[ruleLogin].resetFields();
+      },
+  }
+};
 </script>
 
 <style lang="scss" >
 #loginfirst {
-    background-color:#002b36;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  background-color: #002b36;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   #login {
     width: 420px;
     height: 420px;
@@ -35,12 +79,12 @@ export default {};
     padding: 35px;
     box-sizing: border-box;
     border-radius: 10px;
-    .el-form-item__content{
-        button{
-            width: 100%;
-            background-color: #66b1ff;
-        }
-    }
+    // .el-form-item__content{
+    //     button{
+    //         width: 100%;
+    //         background-color: #66b1ff;
+    //     }
+    // }
   }
 }
 </style>
