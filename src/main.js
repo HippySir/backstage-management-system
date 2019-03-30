3fffb4439e8640efe385e48615f00ba77060c4b0
HippySir <737438933@qq.com>
2019-03-30 11:40:14 +0800

    MUI组件的引入

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 Vue.config.productionTip = false;
 // 导入路由
 import router from './router.js';
+// 导入MUI组件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 new Vue({
   el:"#app",
