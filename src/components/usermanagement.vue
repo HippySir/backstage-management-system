5a4f25a32a5be81da1187a05399ccc3a8727e33e
HippySir <737438933@qq.com>
2019-03-31 13:36:28 +0800

    登录页调整

--- src/components/usermanagement.vue
+++ src/components/usermanagement.vue
@@ -6,13 +6,13 @@
       <el-breadcrumb-item>活动列表</el-breadcrumb-item>
     </el-breadcrumb>
     <!-- 搜索栏和按钮 -->
-    <el-row >
-      <el-col :span="6" >
-        <el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
+    <el-row>
+      <el-col :span="6">
+        <el-input placeholder="请输入内容" v-model="requireparam.query" class="input-with-select">
           <el-button slot="append" icon="el-icon-search"></el-button>
         </el-input>
       </el-col>
-      <el-col :span="12" class=" inputcontent">
+      <el-col :span="12" class="inputcontent">
         <el-button type="primary" plain>主要按钮</el-button>
       </el-col>
       <!-- 表格 -->
@@ -23,55 +23,53 @@
       </el-table>
     </el-row>
     <!-- 分页器 -->
-     <el-pagination
+    <el-pagination
       :page-sizes="[100, 200, 300, 400]"
       :page-size="100"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="400">
-    </el-pagination>
+      :total="400"
+    ></el-pagination>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            tableData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }]
-        }
-           
-    }
+  data() {
+    return {
+      requireparam:{
+        query:"",
+        pagenum:1,
+        pagesize:10,
+      },
+    };
+  },
+  methods: {
+    
+  },
+  async created() {
+    let res = await this.$axios.get('users',{
+      params: this.requireparam,
+       
+      
+    });
+    console.log(res);
+  },
+
 };
 </script>
 
 <style  lang="scss" scoped="" type="text/css">
-    .usermageement{
-        background-color: #e9eef3;
-        .bread{
-            height: 50px;
-            line-height: 50px;
-            padding-left: 10px;
-            padding-right: 0;
-            background-color: #d3dce6;
-           
-        }
-        .inputcontent{
-            padding-left: 10px;
-        }
-    }
+.usermageement {
+  background-color: #e9eef3;
+  .bread {
+    height: 50px;
+    line-height: 50px;
+    padding-left: 10px;
+    padding-right: 0;
+    background-color: #d3dce6;
+  }
+  .inputcontent {
+    padding-left: 10px;
+  }
+}
 </style>
