9644c10aca9d03013971323d947467c775984092
HippySir <737438933@qq.com>
2019-03-31 19:00:12 +0800

    状态栏的改变

--- src/components/usermanagement.vue
+++ src/components/usermanagement.vue
@@ -9,17 +9,46 @@
     <el-row>
       <el-col :span="6">
         <el-input placeholder="请输入内容" v-model="requireparam.query" class="input-with-select">
-          <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-button slot="append" icon="el-icon-search" @click="searchUser"></el-button>
         </el-input>
       </el-col>
       <el-col :span="12" class="inputcontent">
         <el-button type="primary" plain>主要按钮</el-button>
       </el-col>
       <!-- 表格 -->
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-        <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table :data="userList" style="width: 100%" border>
+        <el-table-column type="index" width="50"></el-table-column>
+        <el-table-column prop="username" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
+        <el-table-column prop="mobile" label="电话"></el-table-column>
+        <el-table-column prop="mg_state" label="用户状态">
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"  @change="statuschange(scope.row,scope.$index)"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)"
+              plain
+              icon="el-icon-edit"
+            ></el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+              plain
+              icon="el-icon-delete"
+            ></el-button>
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)"
+              plain
+              icon="el-icon-message"
+            ></el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </el-row>
     <!-- 分页器 -->
@@ -36,25 +65,46 @@
 export default {
   data() {
     return {
-      requireparam:{
-        query:"",
-        pagenum:1,
-        pagesize:10,
+      requireparam: {
+        query: "",
+        pagenum: 1,
+        pagesize: 10
       },
+      userList: [],
+      total: 0,
+     
     };
   },
   methods: {
-    
+    // 点击按钮的搜索函数
+    searchUser(){
+      console.log('nihao');
+    },
+    handleEdit(index, row) {
+      console.log(index);
+      console.log(row);
+    },
+    handleDelete(index, row) {
+      console.log(index);
+      console.log(row);
+    },
+    switchEdit(index,row){
+
+    },
+    // 用户状态改变的方法
+    async statuschange(statuschange,index){
+      let res = await this.$axios.put(`users/${statuschange.id}/state/${statuschange.mg_state}`);
+    }
   },
   async created() {
-    let res = await this.$axios.get('users',{
+    let res = await this.$axios.get("users", {
       params: this.requireparam,
-       
-      
+    
     });
+    this.userList = res.data.data.users;
     console.log(res);
-  },
-
+    this.total = res.data.data.total;
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,26 +1,49 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
 
 // 引入公共样式的文件
-import './assets/base.css';
+import "./assets/base.css";
 Vue.config.productionTip = false;
 
 // 导入路由
-import router from './router.js';
+import router from "./router.js";
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
 
 // 导入axios
-import axios from 'axios';
+import axios from "axios";
 axios.defaults.baseURL = "http://localhost:8888/api/private/v1/";
 Vue.prototype.$axios = axios;
 // 设置基地址
 
-// s
+// // 设置拦截器
+axios.interceptors.request.use(
+  function(config) {
+    config.headers.Authorization = window.sessionStorage.getItem("token");
+    return config;
+  },
+  function(error) {
+  
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function(response) {
+    console.log(response);
+   Vue.prototype.$message.success(response.data.meta.msg);
+    return response;
+  },
+  function(error) {
+    
+    return Promise.reject(error);
+  }
+);
+
 new Vue({
-  el:"#app",
+  el: "#app",
   render: h => h(App),
-  router,
-})
+  router
+});
