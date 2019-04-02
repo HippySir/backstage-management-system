34d890546afaa91e9cfc69a6aa37490805be059f
HippySir <737438933@qq.com>
2019-04-02 14:29:32 +0800

    角色删除功能的完成

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="usermageement">
-   <my-bread secondnav="权限管理" threenav="角色列表"></my-bread>
+    <my-bread secondnav="权限管理" threenav="角色列表"></my-bread>
     <!-- 按钮 -->
     <el-row>
       <el-col :span="12" class="inputroles">
-        <el-button  plain @click="addVisible = true">添加角色</el-button>
+        <el-button plain @click="addRoleVisible = true">添加角色</el-button>
       </el-col>
       <!-- 表格 -->
-      <el-table  :data="roleList" style="width: 100%" border>
+      <el-table
+        :data="roleList"
+        style="width: 100%"
+        border
+        highlight-current-row
+        @current-change="handleCurrentChange"
+      >
         <el-table-column type="index" width="50"></el-table-column>
-        <el-table-column prop="username" label="角色姓名" width="300"></el-table-column>
-        <el-table-column prop="email" label="角色描述" width="300"></el-table-column>
+        <el-table-column prop="roleName" label="角色姓名" width="300"></el-table-column>
+        <el-table-column prop="roleDesc" label="角色描述" width="300"></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button
@@ -36,35 +42,133 @@
         </el-table-column>
       </el-table>
     </el-row>
-   
-  
-  
+    <!-- 添加角色按钮的弹出框 -->
+    <el-dialog title="添加角色" :visible.sync="addRoleVisible">
+      <el-form :model="addRoleInfo" :rules="rolerules">
+        <el-form-item label="角色名称" label-width="100px" prop="roleName">
+          <el-input v-model="addRoleInfo.roleName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" label-width="100px" prop="roleDesc">
+          <el-input v-model="addRoleInfo.roleDesc" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addRoleVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addRoleoPeration">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 编辑角色的弹出框 -->
+    <el-dialog title="添加角色" :visible.sync="editRoleVisible">
+      <el-form :model="addRoleInfo" :rules="rolerules">
+        <el-form-item label="角色名称" label-width="100px" prop="roleName">
+          <el-input v-model="addRoleInfo.roleName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述" label-width="100px" prop="roleDesc">
+          <el-input v-model="addRoleInfo.roleDesc" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editRoleVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editRoleoPeration">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 删除按钮的弹出框 -->
+    <el-dialog title="温馨提示" :visible.sync="deleteroleVisible" width="50%">
+      <span>你确定要删除码这个角色吗？</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="deleteroleVisible = false">取 消</el-button>
+        <el-button type="primary" @click="deleteRoleoperation">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
-
 <script>
 export default {
   data() {
     return {
-     addVisible:false,
-     roleList:[{},{}],
-   
-     
-      
-  };
+      // 添加角色的相关字段
+      addRoleVisible: false,
+      addRoleInfo: {
+        roleName: "",
+        roleDesc: ""
+      },
+      //  添加角色的校验规则
+      rolerules: {
+        roleName: [
+          { required: true, message: "请输入角色名称", trigger: "blur" },
+          { min: 3, max: 16, message: "长度在 3 到 16 个字符", trigger: "blur" }
+        ],
+        roleDesc: [
+          { required: true, message: "请输入角色描述", trigger: "blur" },
+          { min: 3, max: 18, message: "长度在 3 到 18 个字符", trigger: "blur" }
+        ]
+      },
+      // 编辑角色的相关字段
+      editRoleVisible: false,
+      // 删除角色的相关字段
+      deleteroleVisible: false,
+      deleteroleInfo: {},
+      roleList: []
+    };
   },
   methods: {
-    // 分页器的函数
- 
-}
-}
-
+    // 获取角色列表的函数
+    async getRoles() {
+      let res = await this.$axios.get("roles");
+      this.roleList = res.data.data;
+      this.roleList.forEach(function(val) {
+        delete val.children;
+      });
+    },
+    // 表格中某行被选中的时候触发的函数
+    handleCurrentChange() {},
+    // 添加角色的时候触发的相关的函数
+    async addRoleoPeration() {
+      let res = await this.$axios.post("roles", this.addRoleInfo);
+      this.getRoles();
+      this.addRoleVisible = false;
+    },
+    // 编辑相应角色的函数
+    async editRoleoPeration() {
+      let res = await this.$axios.put(`roles/${this.addRoleInfo.id}`, {
+        roleName: this.addRoleInfo.roleName,
+        roleDesc: this.addRoleInfo.roleDesc
+      });
+      if (res.data.meta.status === 200) {
+        this.getRoles();
+      }
+      this.editRoleVisible = false;
+    },
+    handleEdit(index, row) {
+      this.editRoleVisible = true;
+      this.addRoleInfo = row;
+    },
+    // 删除角色的相关的函数
+    handleDelete(index, row) {
+      this.deleteroleInfo = row;
+      this.deleteroleVisible = true;
+    },
+   async deleteRoleoperation() {
+      let res = await this.$axios.delete(`roles/${this.deleteroleInfo.id}`);
+    
+      if(res.data.meta.status === 200 ){
+        this.getRoles();
+        alert('quni de ');
+      };
+      this.deleteroleVisible = false;
+    },
+  },
+  created() {
+    // 页面加载进来的时候加载数据
+    this.getRoles();
+  }
+};
 </script>
 
 <style  lang="scss" scoped="" type="text/css">
 .usermageement {
   background-color: #e9eef3;
- 
+
   .inputcontent {
     padding-left: 10px;
   }
